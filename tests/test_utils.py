d29d18381f61cd85dcd12a6e387d9e378f2e9eef
Tom Matheussen <tmatheussen@avia-gis.com>
2025-10-08 12:14:55 +0000

    Adjustments to test, don't test 0x8A, 0 based

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,34 +2,60 @@ from satel_integra.utils import checksum, decode_bitmask_le, encode_bitmask_le
 
 import pytest
 
-test_frames = [
+# List values, byte data, length
+test_frames: list[tuple[list[int], bytearray, int]] = [
+    # Example from 0x00 command
+    (
+        [3, 14, 128],
+        bytearray(
+            [
+                0x04,
+                0x20,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x80,
+            ]
+        ),
+        16,
+    ),
     # Example from 0x80 command
-    ([1, 2, 29], 4, [0x03, 0x00, 0x00, 0x10]),
+    ([1, 2, 29], bytearray([0x03, 0x00, 0x00, 0x10]), 4),
     # Example from 0x86 command
     (
         [1, 3, 62, 120],
+        bytearray(
+            [
+                0x05,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x20,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x80,
+                0x00,
+            ]
+        ),
         16,
-        [
-            0x05,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x20,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x00,
-            0x80,
-            0x00,
-        ],
     ),
-    # Example from 0x8A command
-    ([4, 63], 8, [0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80]),
 ]
 
 
@@ -42,19 +68,27 @@ def test_checksum_known_value() -> None:
     assert c == 0x8A9B
 
 
-@pytest.mark.parametrize("input,length,expected", test_frames)
-def test_bitmask_bytes_encoding(input, length, expected) -> None:
+@pytest.mark.parametrize("data, expected, length", test_frames)
+def test_bitmask_bytes_encoding(
+    data: list[int],
+    expected: bytearray,
+    length: int,
+) -> None:
     """Test encode_bitmask_le function."""
-    result = encode_bitmask_le(input, length)
+    result = encode_bitmask_le(data, length)
 
     assert isinstance(result, bytes)
-    assert result == bytearray(expected)
+    assert result == expected
 
 
-@pytest.mark.parametrize("expected,length,input", test_frames)
-def test_bitmask_bytes_decoding(expected, length, input) -> None:
+@pytest.mark.parametrize("expected, data, length", test_frames)
+def test_bitmask_bytes_decoding(
+    expected: list[int],
+    data: bytearray,
+    length: int,
+) -> None:
     """Test bitmask_bytes_le function."""
-    result = decode_bitmask_le(bytearray(input), length)
+    result = decode_bitmask_le(data, length)
 
     assert isinstance(result, list)
     assert result == expected
